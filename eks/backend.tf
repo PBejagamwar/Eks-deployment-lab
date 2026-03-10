02f798124d79190f40e47ce1842420e2c5b55008
pratikbejagamwar <PBejagamwar@lofllc.com>
2026-03-10 21:55:45 +0530

    new file

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,19 +1,22 @@
 terraform {
-required_version = "~> 1.14.0"
-required_providers {
-aws = {
-source  = "hashicorp/aws"
-version = "~> 5.99.0"
+  required_version = "~> 1.14.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.99.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "eks-state-lock-bucket"
+    region       = "us-east-1"
+    key          = "eks/terraform.tfstate"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
-}
-backend "s3" {
-bucket         = "eks-state-lock-bucket"
-region         = "us-east-1"
-key            = "eks/terraform.tfstate"
-encrypt        = true
-use_lockfile   = true
-}
-}
+
 provider "aws" {
-region = var.aws-region
+  region = var.aws-region
 }
