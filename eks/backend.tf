3ae8336d55a5165ea6dd74a638b619ae2283c385
pratikbejagamwar <PBejagamwar@lofllc.com>
2026-03-10 21:15:40 +0530

    eks deployment template

---
+++ eks/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.99.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "eks-state-lock-bucket"
+    region         = "us-east-1"
+    key            = "eks/terraform.tfstate"
+    encrypt        = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.aws-region
+}
